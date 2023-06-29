da8adbf518033d6c86cdc8a65c97cf75330ff78f
Towhidul Islam <towhidulislam2.bd@gmail.com>
2023-06-30 00:19:20 +0600

    p[rojec added

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT||5000
-// towhidulislam2bdportfollio
-// WlNF9EiKQdb5eZgr
-app.get('/', (req, res) => {
-  res.send('Towhidulislam is Running')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+app.use(cors());
+app.use(express.json())
+
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Towhidulislam is Running");
+});
+app.get("/test", (req,res)=>{
+  res.send({hello:"gelo"})
 })
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
 
-console.log(process.env.PORTFOLLIO_DB_USER);
-console.log(process.env.PORTFOLLIO_DB_PASS);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "10h",
+  });
+  res.send({ token });
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.PORTFOLLIO_DB_USER}:${process.env.PORTFOLLIO_DB_PASS}@cluster0.w8zzyxt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,24 +52,74 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
     // Send a ping to confirm a successful connection
+    const projectsCollection = client
+      .db("towhidulislam-portfolio")
+      .collection("Projects");
+    const usersCollection = client
+      .db("towhidulislam-portfolio")
+      .collection("user");
+
+      const verifyAdmin = async (req, res, next) => {
+        const email = req.decoded.email;
+        const query = { email: email }
+        const user = await usersCollection.findOne(query);
+        if (user?.role !== 'admin') {
+            return res.status(403).send({ error: true, message: 'forbidden message' });
+        }
+        next();
+    }
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const useremail = req.params.email;
+      const query = { email: useremail };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    // add product
+    app.post("/projects", verifyJWT,verifyAdmin, async(req,res)=>{
+      const project=req.body
+      const result= await projectsCollection.insertOne(project)
+      res.send(result)
+    })
+    app.get("/projects",async (req,res)=>{
+      const result = await projectsCollection.find().toArray()
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
